84dc4f9b34326a1728415d4fbb0f81173126cfd4
Kaelan Cooter <dcgamers@gmail.com>
2014-03-19 20:49:21 -0400

    added more game files; tiles

---
+++ game/js/resources.js
@@ -0,0 +1,14 @@
+/* globals game */
+
+game.resources = [
+    /**
+     * Graphics.
+     */
+    // our level tileset
+    {name: "blue",  type:"image", src: "data/blue.png"},
+     
+    /* 
+     * Maps. 
+     */
+    {name: "map", type: "tmx", src: "data/map.tmx"}
+];
