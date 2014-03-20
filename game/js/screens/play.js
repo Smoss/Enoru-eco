a320c48d6e16cd799337141198f1739cd5dfaccc
Kaelan Cooter <dcgamers@gmail.com>
2014-03-19 21:08:01 -0400

    lolz

--- game/js/screens/play.js
+++ game/js/screens/play.js
@@ -13,8 +13,6 @@ game.PlayScreen = me.ScreenObject.extend({
         game.data.score = 0;
          
         // add our HUD to the game world
-        //this.HUD = new game.HUD.Container();
-        //me.game.world.addChild(this.HUD);
          
     },
      
