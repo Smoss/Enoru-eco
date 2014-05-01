935562c5d7c85819564fc8a8cddd90fc63d3c4a2
Kaelan Cooter <dcgamers@gmail.com>
2014-04-30 20:53:49 -0400

    cleaned up again

--- game/js/entities/shakie.js
+++ game/js/entities/shakie.js
@@ -51,44 +51,6 @@ game.staticEntity = me.ObjectEntity.extend({
         this.updateMovement();
         
         return true;
-        /*if (me.input.isKeyPressed('left')) {
-            // flip the sprite on horizontal axis
-            // update the entity velocity
-            this.vel.x -= this.accel.x * me.timer.tick;
-            this.vel.y = 0;
-        } else if (me.input.isKeyPressed('right')) {
-            // unflip the sprite
-            // update the entity velocity
-            this.vel.x += this.accel.x * me.timer.tick;
-            this.vel.y = 0;
-        }
-        else {
-            this.vel.x = 0;
-        }
-        if (me.input.isKeyPressed('up')) {
-            this.vel.y -= this.accel.y * me.timer.tick;
-            this.vel.x = 0;
-            // set the jumping flage
-        } else if (me.input.isKeyPressed('down')) {
-            this.vel.y += this.accel.y * me.timer.tick;
-            this.vel.x = 0;
-        } else {
-            this.vel.y = 0;
-        }
- 
-        // check & update player movement
-        this.updateMovement();
- 
-        // update animation if necessary
-        if (this.vel.x !== 0 || this.vel.y !== 0) {
-            // update object animation
-            this.parent();
-            return true;
-        }
-         
-        // else inform the engine we did not perform
-        // any update (e.g. position, animation)
-        return false;*/
     },
     /* ---
      
