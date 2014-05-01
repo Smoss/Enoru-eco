80fe03746e36d26e04941255415e77b3870ff1cf
Liam Fratturo <lcheeseboy@gmail.com>
2014-04-30 20:11:09 -0400

    Title screen added.  Dunno why there's no text, you can press enter to get to the game.

--- game/js/main.js
+++ game/js/main.js
@@ -44,16 +44,16 @@ var game = {
     // Run on game resources loaded.
     "loaded": function () {
        // set the "Play/Ingame" Screen Object
-        //me.state.set(me.state.MENU, new game.TitleScreen());
+        me.state.set(me.state.MENU, new game.TitleScreen());
       
         // set the "Play/Ingame" Screen Object
-        //me.state.set(me.state.PLAY, new game.PlayScreen());
+        me.state.set(me.state.PLAY, new game.PlayScreen());
         //me.pool.register("mainPlayer", game.PlayerEntity);
         //me.pool.register("CoinEntity", game.CoinEntity);
         //me.pool.register("EnemyEntity", game.EnemyEntity);
 
         //me.state.set(me.state.MENU, new game.TitleScreen());
-        me.state.set(me.state.PLAY, new game.PlayScreen());
+        //me.state.set(me.state.PLAY, new game.PlayScreen());
 
 
         me.entityPool.add("mainPlayer", game.PlayerEntity);
@@ -66,16 +66,60 @@ var game = {
         me.input.bindKey(me.input.KEY.UP,    "up");
  
         // Start the game.
-        me.state.change(me.state.PLAY);
+        me.state.change(me.state.MENU);
     }
 };
 
 game.TitleScreen = me.ScreenObject.extend({
-    // reset function
-    onResetEvent: function() {
+    onResetEvent: function() {  
+        // title screen
+        me.game.world.addChild(
+            new me.SpriteObject (
+                0,0, 
+                me.loader.getImage('title_screen')
+            ),
+            1
+        );
+
+        // add a new renderable component with the scrolling text
+        me.game.world.addChild(new (me.Renderable.extend ({
+            // constructor
+            init : function() {
+                this.parent(new me.Vector2d(0, 0), me.game.viewport.width, me.game.viewport.height);
+                // font for the scrolling text
+                this.font = new me.BitmapFont("32x32_font", 32);
+            },
+
+            update : function (dt) {
+                return true;
+            },
+             
+            draw : function (context) {
+                this.font.draw (context, "PRESS ENTER TO PLAY", 20, 240);
+            },
+            onDestroyEvent : function() {
+            }
+        })), 2);
+
+        // change to play state on press Enter or click/tap
+        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+        //me.input.bindPointer(me.input.mouse.LEFT, me.input.KEY.ENTER);
+        this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge) {
+            if (action === "enter") {
+                // play something on tap / enter
+                // this will unlock audio on mobile devices
+                me.state.change(me.state.PLAY);
+            }
+        });
     },
- 
-    // destroy function
+    
+    
+    /** 
+     *  action to perform when leaving this screen (state change)
+     */
     onDestroyEvent: function() {
+        me.input.unbindKey(me.input.KEY.ENTER);
+        //me.input.unbindPointer(me.input.mouse.LEFT);
+        me.event.unsubscribe(this.handler);
     }
 });
